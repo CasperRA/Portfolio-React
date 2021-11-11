9d0df51f77618ea3484ad57124bfc5ecfdcf0ce2
Casper <casp8249@edu.easj.dk>
2021-11-11 22:46:33 +0100

    Slider

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,16 +11,32 @@ function Projects() {
   }, []);
   return (
     <div>
-      <ScrollContainer className="scroll-container">
-        <div>
-          <h1>Nice</h1>
-        </div>
-        <div>
-          <h1>Nice</h1>
-        </div>
-        <div>
-          <h1>Nice</h1>
-        </div>
+      <ScrollContainer className="carousel">
+        <Link draggable="false" to="/NaestvedProvsti">
+          <div className="carouselItem case1">
+            <h2>Næstved Provsti</h2>
+          </div>
+        </Link>
+        <Link draggable="false" to="/item2">
+          <div className="carouselItem case2">
+            <h2>Tension Therapy</h2>
+          </div>
+        </Link>
+        <Link draggable="false" to="/item3">
+          <div className="carouselItem case3">
+            <h2>Case 3</h2>
+          </div>
+        </Link>
+        <Link draggable="false" to="/item4">
+          <div className="carouselItem case4">
+            <h2>Case 4</h2>
+          </div>
+        </Link>
+        <Link draggable="false" to="/item5">
+          <div className="carouselItem case5">
+            <h2>Case 5</h2>
+          </div>
+        </Link>
       </ScrollContainer>
     </div>
   );
